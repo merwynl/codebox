b6b4e350674c1f122743fc5d859115bec34c2783
まーういん <merwyn.lim@gmail.com>
2025-07-14 22:14:37 +1000

    updates functions arguments & comments

--- docs/04_functions/01_functions.py
+++ docs/04_functions/01_functions.py
@@ -1,45 +1,27 @@
 """
-[summary]
+A block of resuable code
+Functions are declared using the def signifier:
+def function_name():
+	some_code
+function_name()
+place () after the function name to invoke it.
 """
 
-# simple function that takes in two inputs and adds them together
+# Simple function that takes in two inputs and adds them together
 def add_numbers(a, b):
-	"""
-	[summary]
-
-	:param a: [description]
-	:type a: [type]
-	:param b: [description]
-	:type b: [type]
-	"""
 	print(a + b)
 
-
 # Function for printing a hello statement
-def hello():
-	"""
-	[summary]
-	"""
-	print('Hello')
+def hello(name):
+	print(f'Hello {name}!')
 
-def names(name, age):
-	"""
-	[summary]
+# Passing in multiple arguments to a function. Order matters.
+def greeting(name, age):
+	print(f'Hello {name}! Happy {age} years old to you!')
 
-	:param name: [description]
-	:type name: [type]
-	:param age: [description]
-	:type age: [type]
-	"""
-	print('こんにちは' + name + 'さん。君は' + age + '歳ですね')
+# Function declaration to display the amount due on an invoice
+def display_invoice(username, amount, due_date):
+	print (f'Hello {username}!')
+	print(f'Your bill of ${amount:.2f} is due: {due_date}')
 
-def greet(name, version):
-	"""
-	[summary]
-
-	:param name: [description]
-	:type name: [type]
-	:param version: [description]
-	:type version: [type]
-	"""
-	print('こんにちは' + name + 'さん。君は' + age + '歳ですね')
+# 
--- docs/04_functions/03_function_types.py
+++ docs/04_functions/03_function_types.py
@@ -5,27 +5,13 @@
 
 # simple function that takes in two inputs and adds them together
 def add_numbers(a, b):
-    """[summary]
-
-	Arguments:
-		a {[type]} -- [description]
-		b {[type]} -- [description]
-	"""
     print(a + b)
-
-
 # Function for printing a hello statement
 def hello():
     print('Hello')
 
 
 def names(name, age):
-    """[summary]
-
-	Arguments:
-		name {[type]} -- [description]
-		age {[type]} -- [description]
-	"""
     print('こんにちは' + name + 'さん。君は' + age + '歳ですね')
 
 
@@ -35,10 +21,4 @@ names('れをる', '二十三')
 
 
 def greet(name, version):
-    """[summary]
-
-	Arguments:
-		name {[type]} -- [description]
-		age {[type]} -- [description]
-	"""
     print(f"This is {name} {str(version)}")
