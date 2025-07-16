5399df10213381cdf159f2bd1fa76480efb6713e
まーういん <merwyn.lim@gmail.com>
2025-07-16 16:32:08 +1000

    update entries on class variables & inheritance. fix file name typo

--- docs/06_classes/02_class_variables.py
+++ docs/06_classes/02_class_variables.py
@@ -1,11 +1,66 @@
 '''
 
 Class variables = shared amongst all instances of a class
-                  defined outside the constructor
+                  Defined outside the constructor
                   Allow you to share data among all objects created from that class.
 
 '''
 
 
+class Student:
+    # Self refers to the object we're currently working with. Used to access vars that belong to the class.
 
-# Class essentially allows us to create our own data type
+    # Declaring a class var. All objects that invoke this class will have access to all these vars.
+    class_year = 2024
+    num_students = 0
+
+    # Example of a constructor
+    # __init__ are always executed when a class is initialised.
+    def __init__(self, name, age, class_year=2025, major="English", gpa=4.5, is_on_probation=False):
+        self.name = name
+        self.age = age
+        self.major = major
+        self.gpa = gpa
+        self.is_on_probation = is_on_probation
+
+        # This will overwrite the class var declared outside the constructor
+        self.class_year = class_year
+
+        # Accessing a class variable to modify
+        Student.num_students += 1
+
+    # Function for determining the honor roll
+    def on_honor_roll(self):
+        if self.gpa >= 3.5:
+            return True
+        else:
+            return False
+
+# Invoking a class object and providing some attributes
+student1 = Student("Adam", "20", is_on_probation=True, major="CS")
+student2 = Student("Lisa", "23", gpa=4.5, major="Liberal Arts")
+student3 = Student("Ken", "21", gpa=5.0, major="Classical History")
+student4 = Student("Sara", "25", gpa=4.25, major="Literature")
+
+
+# Calling and displaying specific class attributes.
+print(student1.is_on_probation)
+print(student1.gpa)
+
+# Accessing object variables and displaying it
+print(student2.name)
+print(student2.gpa)
+
+# Accessing a class variable through an object.
+# This returns the value of the object var declared within the constructor.
+print(student2.class_year)
+
+# Accessing a class variable through the name of the class
+# Better to access a class variable through the name of the class rather than an object.
+# More explicit and it helps with clarity and readability.
+print(Student.class_year)
+
+# Accessing a class variable that has been modified in the constructor.
+print(Student.num_students)
+
+print(f"My graduating class of {Student.class_year} has {Student.num_students} students.")
--- docs/06_classes/03_inheritance.py
+++ docs/06_classes/03_inheritance.py
@@ -1,5 +1,47 @@
 '''
 
-Inheritance
+Inheritance - Allows a class to inherit attributes and methods from another class
+              Helps with code reusability and extensibility
+              class Child(Parent)
 
 '''
+
+# Creating a parent class
+class Animal:
+    def __init__(self, name):
+        self.name = name
+        self.is_alive = True
+
+    def eat(self):
+        print(f"{self.name} is eating")
+
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+
+# Creating a child class of type dog. This class will inherit all attributes from the parent class.
+class Dog(Animal):
+    def speak(self):
+        print("Woof!")
+
+class Cat(Animal):
+    def speak(self):
+        print("Meow!")
+
+class Mouse(Animal):
+    pass
+
+dog = Dog("Rover")
+cat = Cat("Tom")
+mouse = Mouse("Jerry")
+
+# Printing the results of an inherited value
+print(dog.name)
+print(dog.is_alive)
+
+# Calling methods from an inherited class
+cat.eat()
+cat.sleep()
+
+#  Calling different methods from child classes
+dog.speak()
+cat.speak()
