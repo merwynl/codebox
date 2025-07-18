bea24e6e4c0042e7c4ef8b39c3b4c267603b6117
merwyn「まーういん」 <merwyn.lim@gmail.com>
2025-07-18 11:55:35 +1000

    Update entries on decorators and exception handling

--- docs/06_classes/12_decorators.py
+++ docs/06_classes/12_decorators.py
@@ -1,5 +1,43 @@
 '''
 
-Decorators
-
+Decorators - A function that extends the behaviour of another function
+           - w/o modifying the base function
+           - Pass the base function as an argument to the decorator
+           - Think of decorators as additional dressing on top of a salad
+           @add_sprinkles
+           get_ice_cream("vanilla")
 '''
+
+# Creating a decorator
+def add_sprinkles(func):
+    # Without a wrapper function, both add_sprinkles() & get_ice_cream() will be instantly called when decorator is applied.
+    def wrapper(*args, **kwargs):
+        print("*Sprinkles added 🎊")
+        func(*args, **kwargs)
+    return wrapper # Returning a whole function.
+
+def add_fudge(func):
+    # If the main function takes in arguments, pass in args & kwargs to take in any number of arguments.
+    # Type error will occur if this is not done.
+    def wrapper(*args, **kwargs):
+        print("*You have added fudge 🍫")
+        func(*args, **kwargs) # Referencing a function requires the same number of args & kwargs as the wrapper.
+    return wrapper
+
+# Invoking the decorator. You can apply more than one decorator to a function
+@add_sprinkles
+@add_fudge
+def get_ice_cream(flavour):
+    print(f"Here is your {flavour} ice cream 🍨")
+
+# Calling on a function that has two decorators applied
+get_ice_cream("Chocolate")
+
+# Showcasing the use of a decorator without a wrapper.
+def add_decorator(func):
+    print ("This is from the decorator")
+    func()
+
+@add_decorator
+def main ():
+    print("This is the main function body")
--- docs/07_exceptions_files/01_exception_handling.py
+++ docs/07_exceptions_files/01_exception_handling.py
@@ -1,22 +1,31 @@
 '''
 
-Exception handling
+Exception handling - An event that interrupts the flow of a program
+                   - (ZeroDivision, TypeError, ValueError)
+                   - Try, Except, Finally
 
 '''
 
-
-# Try's & excepts are useful for catching any potential errors & prevents a program from crashing
+# Try's & excepts are useful for catching any potential errors & prevents a program from crashing.
 try:
     # answer = 10/0
     number = int(input('数字を入力してください: '))
-    print(number)
+    print(1 / number)
 
-# Without a specified clause, it will catch any and all errors. ZeroDivisionError catches if a return value is 0
+# Without a specified clause, it will catch any and all errors. ZeroDivisionError catches if a return value is 0.
 except ZeroDivisionError as err:
     print('ゼロから割れ切れています')
-    print(err)
 
-# Without a specified clause, it will catch any and all errors
+# Without a specified clause, it will catch any and all errors.
 except ValueError:
-    print('申し訳ございます。それは数字ではありません')
+    print('申し訳ございます。それは数字ではありません!')
+
+# Catches all exceptions. Considered bad practice as it's too broad and vague. Last resort.
+except Exception:
+    print("Something went wrong.")
+
+# Always executes. Usually used to perform cleanup operations. Not always used.
+finally:
+    print("Do some clean up here.")
+
 
