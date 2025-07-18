568193f06ed2a75c4f5378f4be781173fcfa202e
merwyn「まーういん」 <merwyn.lim@gmail.com>
2025-07-18 15:39:06 +1000

    Add example on multithreading & api access

--- docs/07_exceptions_files/07_multithreading.py
+++ docs/07_exceptions_files/07_multithreading.py
@@ -1,6 +1,41 @@
 '''
 
-Multi threading
+Multi threading - Used to perform multiple takss concurrently (multitasking)
+                - Good for I/O bound tasks like reading files and fetching data from API's
+                - threading.Thread(target = my_function)
 
 '''
 
+import threading
+import time
+
+# Functions running on a single main thread
+def walk_dog(name):
+    time.sleep(8)
+    print(f"You finished walking {name}.")
+
+def take_out_trash():
+    time.sleep(4)
+    print("You took out the trash.")
+
+def get_mail():
+    time.sleep(2)
+    print("You've got mail")
+
+# Initializing multithreading on several functions
+# Applying arguments to a multithreaded task. args, take in a tuple but must include a , if only one arg is given
+chore1 = threading.Thread(target=walk_dog, args=("Rover",))
+chore1.start()
+
+chore2 = threading.Thread(target=take_out_trash)
+chore2.start()
+
+chore3 = threading.Thread(target=get_mail())
+chore3.start()
+
+# Prevents any other code after multithreading to finish before the current tasks
+chore1.join()
+chore2.join()
+chore3.join()
+
+print("All chores are complete!")
--- docs/07_exceptions_files/08_request_api_data.py
+++ docs/07_exceptions_files/08_request_api_data.py
@@ -1,6 +1,28 @@
 '''
 
-Request API Data
+Request API Data - https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status
 
 '''
 
+import requests
+
+base_url = "https://pokeapi.co/api/v2/"
+
+def get_pokemon_info(name):
+    url = f"{base_url}/pokemon/{name}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        return pokemon_data
+    else:
+        return f"Failed to retrieve data {response.status_code}"
+
+pokemon_name = "bulbasaur"
+pokemon_info = get_pokemon_info(pokemon_name)
+
+if pokemon_info:
+    print(f"Name: {pokemon_info["name"].capitalize()}")
+    print(f"ID: {pokemon_info["id"]}")
+    print(f"Height: {pokemon_info["height"]}")
+    print(f"Weight: {pokemon_info["weight"]}")
